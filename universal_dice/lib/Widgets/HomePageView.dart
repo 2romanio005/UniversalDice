23e9a321232e9cb00ac086a3b0ccba4a1a819c47
Roman_Proskuryakov <2romanio005@gmail.com>
2024-04-15 21:08:20 +0300

    it is rotating

--- universal_dice/lib/Widgets/HomePageView.dart
+++ universal_dice/lib/Widgets/HomePageView.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 import 'package:universal_dice/Decoration/styles.dart';
 
@@ -20,47 +21,57 @@ class HomePageView extends StatefulWidget {
   List<OneSelectedDiceGroup> allSelectedDiceGroup;
 }
 
-class _HomePageView extends State<HomePageView> {
+class _HomePageView extends State<HomePageView> with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+    _animation = Tween<double>(begin: 0, end: pi * 4).animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     const double padding = 20;
     return widget.allSelectedDiceGroup.isEmpty
-        ?
-    const Padding(
-      padding: EdgeInsets.fromLTRB(padding, 100, padding, 0),
-      child: Text(
-        "Выберите какие кубики кидать, нажав на кнопку в верхнем (правом или левом) углу.",
-        textAlign: TextAlign.center,
-      ),
-    ) :
-    Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Column(
-          children: [
-            Expanded(child: _buildSelectedDiceGroupList()),
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 12),
-              child: TextButton(
-                style: buttonStyleDefaultInverted,
-                onPressed: () {
-                  for (OneSelectedDiceGroup selectedDiceGroup in widget.allSelectedDiceGroup){
-                    for (Dice dice in selectedDiceGroup.allDice){
-                      dice.generateRandFaceIndex();
-                    }
-                  }
-                  setState(() {});
-                },
-                child: const FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text("Бросить все игральные кости"),
-                ),
+        ? const Padding(
+            padding: EdgeInsets.fromLTRB(padding, 100, padding, 0),
+            child: Text(
+              "Выберите какие кубики кидать, нажав на кнопку в верхнем (правом или левом) углу.",
+              textAlign: TextAlign.center,
+            ),
+          )
+        : Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Expanded(child: _buildSelectedDiceGroupList()),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 5, 0, 12),
+                    child: TextButton(
+                      style: buttonStyleDefaultInverted,
+                      onPressed: () {
+                        for (OneSelectedDiceGroup selectedDiceGroup in widget.allSelectedDiceGroup) {
+                          for (Dice dice in selectedDiceGroup.allDice) {
+                            dice.generateRandFaceIndex();
+                          }
+                        }
+                        _controller.forward(from: 0);
+                        setState(() {});
+                      },
+                      child: const FittedBox(
+                        fit: BoxFit.fitWidth,
+                        child: Text("Бросить все игральные кости"),
+                      ),
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
-        ),
-      ],
-    );
+            ],
+          );
   }
 
   Widget _buildSelectedDiceGroupList() {
@@ -89,11 +100,36 @@ class _HomePageView extends State<HomePageView> {
     diceFaceDimension -= diceFacePadding;
 
     Widget buildDiceFace(Dice dice, [EdgeInsetsGeometry? padding]) {
-      return dice.getFace(
-        dimension: diceFaceDimension,
-        index: dice.lastRandFaceIndex,
-        padding: padding ?? EdgeInsets.only(bottom: diceFacePadding),
+      return AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: _animation.value,
+            child: Container(
+              child: dice.getFace(
+                dimension: diceFaceDimension,
+                index: dice.lastRandFaceIndex,
+                padding: padding ?? EdgeInsets.only(bottom: diceFacePadding),
+              ),
+            ),
+          );
+        },
       );
+      // return TweenAnimationBuilder(
+      //   tween: tween,
+      //   duration: duration,
+      //   builder: (){
+      //
+      //   },
+      // );
+      // return RotationTransition(
+      //   turns: _animation,
+      //   child: dice.getFace(
+      //     dimension: diceFaceDimension,
+      //     index: dice.lastRandFaceIndex,
+      //     padding: padding ?? EdgeInsets.only(bottom: diceFacePadding),
+      //   ),
+      // );
     }
 
     return Column(
